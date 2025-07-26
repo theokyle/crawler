88373c8a554f995eb0cac6ef778845d80cd22b46
theokyle <theokyle@gmail.com>
2025-07-26 11:41:12 -0500

    add cli to main func

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) <= 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	base_url := os.Args[1]
+	fmt.Printf("starting crawl of: %s\n", base_url)
 }
